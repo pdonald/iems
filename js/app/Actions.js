fbc4e082884c030c97814905472495f56e90d988
Pēteris Ņikiforovs <p@mak.id.lv>
2015-09-10 23:10:49 +0300

    switch to babel

---
+++ js/app/Actions.js
@@ -0,0 +1,11 @@
+var Actions = {
+  add: Reflux.createAction(),
+  delete: Reflux.createAction(),
+  move: Reflux.createAction(),
+  connect: Reflux.createAction(),
+  select: Reflux.createAction(),
+  goIntoGroup: Reflux.createAction(),
+  portSelected: Reflux.createAction(),
+  portDeselected: Reflux.createAction(),
+  paramChanged: Reflux.createAction(),
+};
