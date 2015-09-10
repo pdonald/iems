fd284755c563002e1a84aa88f868210c22740d06
Pēteris Ņikiforovs <p@mak.id.lv>
2015-09-10 17:37:58 +0300

    change select to dbl click

--- js/graph/Process.js
+++ js/graph/Process.js
@@ -59,7 +59,7 @@ var Process = React.createClass({
                  pos={{x: this.props.x, y: this.props.y}} min={min} max={max}
                  onMove={this.onMove}>
         <g>
-          <rect x="0" y="0" width={width} height={height} onClick={this.onClick}/>
+          <rect x="0" y="0" width={width} height={height} onDoubleClick={this.onClick}/>
           <g onClick={this.goIntoGroup}><text x="10" y="30">{!this.props.blank ? this.props.name : ''}</text></g>
           <g>{ports.in.map((port, index) => <Port process={this.props.id} key={port} label={this.portName(this.props.graph, 'input', port)} type="in" x={(index+1)*offset.x} y={0}/>)}</g>
           <g>{ports.out.map((port, index) => <Port process={this.props.id} key={port} label={this.portName(this.props.graph, 'output', port)} type="out" x={(index+1)*offset.y} y={height}/>)}</g>
