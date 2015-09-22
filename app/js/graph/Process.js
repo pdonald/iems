49bd9502ea73901768e3fd50ec657c7644bf89f1
Pēteris Ņikiforovs <p@mak.id.lv>
2015-09-23 00:26:42 +0300

    resize corners

--- app/js/graph/Process.js
+++ app/js/graph/Process.js
@@ -44,13 +44,26 @@ var Process = React.createClass({
     var padding = 10;
     var min = { x: padding, y: padding };
 
+    var resize;
+    if (!this.props.blank) {
+      resize = (
+        <g>
+          <rect className="resize" style={{cursor: "nw-resize"}} x={0} y={0} width={10} height={10}/>
+          <rect className="resize" style={{cursor: "ne-resize"}} x={width-10} y={0} width={10} height={10}/>
+          <rect className="resize" style={{cursor: "sw-resize"}} x={0} y={height-10} width={10} height={10}/>
+          <rect className="resize" style={{cursor: "se-resize"}} x={width-10} y={height-10} width={10} height={10}/>
+        </g>
+      );
+    }
+
     return (
       <Draggable className={classes.join(' ')}
                  pos={{x: this.props.x, y: this.props.y}} min={min}
                  onMove={this.onMove}>
         <g className={this.props.graph.collapsed?'zoom-in':''}>
-          <rect className="process-rect" x="0" y="0" width={width} height={height} onDoubleClick={this.goIntoGroup}/>
+          <rect className="process-rect" x={0} y={0} width={width} height={height} onDoubleClick={this.goIntoGroup}/>
           <text x="10" y="30" onDoubleClick={this.goIntoGroup}>{this.props.title}</text>
+          {resize}
           <g>{ports.input.map((port, index) => <Port process={this.props.graph} group={this.props.group} key={port} port={port} label={this.portName(this.props.graph, 'input', port)} type="input" x={(index+1)*offset.x} y={0}/>)}</g>
           <g>{ports.output.map((port, index) => <Port process={this.props.graph} group={this.props.group} key={port} port={port} label={this.portName(this.props.graph, 'output', port)} type="output" x={(index+1)*offset.y} y={height}/>)}</g>
         </g>
