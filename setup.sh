93757f5935ed4ee9238bbe1c0a8eaf8ac3b90a9b
Pēteris Ņikiforovs <p@mak.id.lv>
2015-09-22 21:22:28 +0300

    static kenlm & fast align

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 export TOOLS=/tools
 export WORK=$HOME
 
@@ -7,7 +9,7 @@ sudo apt-get -qq update
 sudo apt-get -qq install htop unzip pigz wget curl coreutils
 sudo apt-get -qq install build-essential cmake git automake libtool flex
 sudo apt-get -qq install zlib1g-dev libboost-all-dev libbz2-dev liblzma-dev python-dev
-sudo apt-get -qq install libboost-all-dev libgoogle-perftools-dev libsparsehash-dev libeigen3-dev gtest-dev google-mock
+sudo apt-get -qq install libboost-all-dev libgoogle-perftools-dev libsparsehash-dev libeigen3-dev
 sudo apt-get -qq install graphviz imagemagick
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
@@ -27,7 +29,7 @@ make install
 cd $WORK
 git clone --depth=1 https://github.com/moses-smt/mosesdecoder
 cd mosesdecoder
-./bjam -a -j`nproc` --with-mm --with-cmph=$WORK/cmph-2.0/build
+./bjam -a -j`nproc` --with-mm --with-cmph=$WORK/cmph-2.0/build --static
 mkdir -p $TOOLS/moses && find bin -maxdepth 1 -type f -executable -exec cp {} $TOOLS/moses \;
 cp -r scripts $TOOLS/moses
 
@@ -43,6 +45,7 @@ mkdir -p $TOOLS/mgiza && cp bin/* $TOOLS/mgiza
 cd $WORK
 git clone --depth=1 https://github.com/clab/fast_align
 cd fast_align
+echo 'SET(CMAKE_EXE_LINKER_FLAGS "-static")' >> CMakeLists.txt
 mkdir build
 cd build
 cmake ..
@@ -52,7 +55,7 @@ mkdir -p $TOOLS/fast_align && cp atools fast_align $TOOLS/fast_align
 cd $WORK
 git clone --depth=1 https://github.com/kpu/kenlm
 cd kenlm
-./bjam -a -j`nproc`
+./bjam -a -j`nproc` --static
 mkdir -p $TOOLS/kenlm && find bin -maxdepth 1 -type f -executable -exec cp {} $TOOLS/kenlm \;
 
 cd $WORK
