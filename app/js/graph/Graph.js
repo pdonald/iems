85eaa0c995fbe90be56811f8c6e95924c59f817e
Pēteris Ņikiforovs <p@mak.id.lv>
2015-09-30 22:13:05 +0300

    make svg 100%, better for drag'n'drop

--- app/js/graph/Graph.js
+++ app/js/graph/Graph.js
@@ -3,8 +3,12 @@ var Graph = React.createClass({
 
   render: function() {
     var size = this.props.graph.getCalculatedSize();
+    var style = {
+      width: '100%', minWidth: (size.width+25)+'px',
+      height: '100%', minHeight: (size.height+100)+'px'
+    };
     return (
-      <svg style={{width: (size.width+25)+'px', height: (size.height+100)+'px'}}>
+      <svg style={style}>
         <Select width={size.width+25} height={size.height+100}>
           {this.props.children}
         </Select>
