b105a081c3f885192d379226e247e621ca4c6201
Pēteris Ņikiforovs <p@mak.id.lv>
2015-09-30 21:38:07 +0300

    refactor drag&drop
    works on ff

--- app/js/Toolbox.js
+++ app/js/Toolbox.js
@@ -1,18 +1,41 @@
 var Toolbox = React.createClass({
   mixins: [React.addons.PureRenderMixin],
-  
+
   getInitialState: function() {
-    return { dragging: null };
+    return {
+      dragging: null,
+      x: 0, y: 0
+    };
+  },
+
+  componentWillMount: function() {
+    document.removeEventListener('mousemove', this.drag);
+    document.removeEventListener('mouseup', this.dragEnd);
   },
 
   dragStart: function(e, obj) {
-    // todo: set image
-    this.setState({ dragging: obj });
+    var $dragdiv = $(`<div class="dragobj">${obj.title || obj.type}</div>`).appendTo('body');
+
+    this.setState({
+      dragging: obj,
+      $dragdiv: $dragdiv,
+    });
+
+    document.addEventListener('mousemove', this.drag);
+    document.addEventListener('mouseup', this.dragEnd);
+  },
+
+  drag: function(e) {
+    this.state.$dragdiv.offset({ left: e.pageX-20, top: e.pageY-20 })
   },
 
   dragEnd: function(e) {
+    document.removeEventListener('mousemove', this.drag);
+    document.removeEventListener('mouseup', this.dragEnd);
+
     if (this.state.dragging) {
-      Actions.add(this.state.dragging, e.pageX, e.pageY)
+      this.state.$dragdiv.remove();
+      Actions.add(this.state.dragging, e.pageX-20, e.pageY-20)
       this.setState({ dragging: null });
     }
   },
@@ -30,10 +53,7 @@ var Toolbox = React.createClass({
           <h3>{CategoryTitles[cat] || cat}</h3>
           <ul>
             {all.filter(p => p.category == cat).map(p => (
-              <li key={cat + '/' + p.type}
-                draggable="true"
-                onDragStart={(e) => this.dragStart(e, p)}
-                onDragEnd={this.dragEnd}>
+              <li key={cat + '/' + p.type} onMouseDown={e => this.dragStart(e, p)}>
                 {p.title || p.type}
               </li>
             ))}
