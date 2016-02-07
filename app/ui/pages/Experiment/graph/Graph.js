ace5858d5cf9e3637c287c4b31b3c7a9ce205db5
Pēteris Ņikiforovs <p@mak.id.lv>
2016-02-07 23:20:57 +0200

    make svg large for empty experiment

--- app/ui/pages/Experiment/graph/Graph.js
+++ app/ui/pages/Experiment/graph/Graph.js
@@ -6,14 +6,31 @@ import Select from './Select'
 export default React.createClass({
   mixins: [PureRenderMixin],
 
+  getInitialState: function() {
+    return { parentHeight: null };
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('resize', this.calculateContainerSize);
+    this.calculateContainerSize();
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener('resize', this.calculateContainerSize);
+  },
+
+  calculateContainerSize: function() {
+    this.setState({ parentHeight: this.refs.svg.parentNode.clientHeight });
+  },
+
   render: function() {
     var size = this.props.graph.getCalculatedSize();
     var style = {
       minWidth: (size.width+25)+'px',
-      minHeight: (size.height+100)+'px'
+      minHeight: Math.max(size.height+100, this.state.parentHeight-5)+'px'
     };
     return (
-      <svg style={style}>
+      <svg style={style} ref="svg">
         <Select width={size.width+25} height={size.height+100}>
           {this.props.children}
         </Select>
