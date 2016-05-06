d0cac8f399bb1ff5c1d5a2b7ac92dacc16124d79
Pēteris Ņikiforovs <p@mak.id.lv>
2016-05-06 13:14:29 +0300

    remove typings

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="react/react.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="react-router/history.d.ts" />
-/// <reference path="react-router/react-router.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
