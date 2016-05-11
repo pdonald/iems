9f62723dfd8c1f690bbf7768628c8f993d7fa00b
Pēteris Ņikiforovs <p@mak.id.lv>
2016-05-11 12:03:29 +0300

    collapsable sidebar blocks

--- app/client/pages/experiment/sidebar/block.tsx
+++ app/client/pages/experiment/sidebar/block.tsx
@@ -1,22 +1,34 @@
 import * as React from 'react'
 
 export default class Block extends React.Component<Props, any> {
+  constructor(props) {
+    super(props)
+    this.state = { collapsed: false }
+  }
+  
   render() {
     if (this.props.hidden)
       return null
       
     return (
       <div className={"block " + this.props.name}>
-        <h2>{this.props.title}</h2>
-        {this.props.children}
+        <h2 onClick={this.handleTitleClick.bind(this)}>
+          {this.props.title}
+          {this.state.collapsed && <small>{' (+)'}</small>}
+        </h2>
+        {!this.state.collapsed && this.props.children}
       </div>
     )
+  }
+  
+  handleTitleClick(e) {
+    this.setState({ collapsed: !this.state.collapsed })
   }
 }
 
 interface Props {
-   name: string;
-   title: string;
-   children?: any;
-   hidden?: boolean;
+   name: string
+   title: string
+   children?: any
+   hidden?: boolean
 }
